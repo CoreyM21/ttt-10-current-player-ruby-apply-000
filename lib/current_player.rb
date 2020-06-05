601ebab1ecd8fed4957a0b4819e6d1f2218ff4cf
Corey <coreyjmartin2@gmail.com>
2020-06-05 03:09:13 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,10 @@
-def turn_count
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  board.each do |move|
-  counter == 0
-  if board == 0 
-    counter += 1
-  else 
-    counter
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if (spaces == "X") || (spaces == "O")
+         counter += 1
+      end
+   end
+   counter
+end
     
